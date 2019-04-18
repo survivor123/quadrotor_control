8c7b8d2b9b38579db952e5af69bc971adaeaf15d
luis <efed@hotmail.com>
2019-04-18 17:03:47 +0200

    initializing workspace

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/luis/Desktop/documents/drone_proof/src
+# Build directory: /home/luis/Desktop/documents/drone_proof/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
